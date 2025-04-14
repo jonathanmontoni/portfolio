2a78df75af9ea6e2c7ddbbc07e754852ce63443b
Jonathan Montoni <Jonathanborgesmoreira@gmail.com>
2025-04-14 16:41:53 -0300

    initial commit

--- business-portfolio/js/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Frontend", "Backend", "Consultoria em TI", "Sistemas", "Automações", "Apps Móveis"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
